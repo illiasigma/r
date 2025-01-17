c18ff418fe96309c458cd8ca3c1024229fcd0f9e
illiasigma <illiaivanychuk28@gmail.com>
2025-01-17 22:24:58 +0200

    Add files via upload

---
+++ fffx.py
@@ -0,0 +1,16 @@
+print('введіть діапазон послідовності')
+b=int(input('від:'))
+z=int(input('до:'))
+for n in range(b,z):
+a=3**n
+print(a)print('введіть діапазон послідовності')
+b=int(input('від:'))
+z=int(input('до:'))
+for n in range(b,z):
+a=3**n
+print(a)print('введіть діапазон послідовності')
+b=int(input('від:'))
+z=int(input('до:'))
+for n in range(b,z):
+a=3**n
+print(a)
